a532ac1a46754f545f2d369e14cdfe38a68b4884
Jpadoliveiragit <Jpadoliveira@minha.fag.edu.br>
2025-11-26 19:20:11 -0300

    Commit 5 - Refactoring 4 (Replace Conditional With Polymorphism)

--- AulaPraticaRefactoring/src/Movie.java
+++ AulaPraticaRefactoring/src/Movie.java
@@ -1,4 +1,45 @@
-
 public class Movie {
 
+    public static final int REGULAR = 0;
+    public static final int NEW_RELEASE = 1;
+    public static final int CHILDRENS = 2;
+
+    private String title;
+    private Price price;
+
+    public Movie(String title, int priceCode) {
+        this.title = title;
+        setPriceCode(priceCode);
+    }
+
+    public int getPriceCode() {
+        return price.getPriceCode();
+    }
+
+    public void setPriceCode(int priceCode) {
+        switch (priceCode) {
+            case REGULAR:
+                price = new RegularPrice();
+                break;
+            case NEW_RELEASE:
+                price = new NewReleasePrice();
+                break;
+            case CHILDRENS:
+                price = new ChildrensPrice();
+                break;
+        }
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public double getAmount(int daysRented) {
+        return price.getAmount(daysRented);
+    }
+
+    public int getFrequentRenterPoints(int daysRented) {
+        return price.getFrequentRenterPoints(daysRented);
+    }
 }
+
